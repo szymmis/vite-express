e12491ba7479d9087961958b520ba8d994bb4a53
Szymon Miśtal <mistalzaq@gmail.com>
2022-12-10 20:32:50 +0100

    Expose static files serving middleware

--- lib/main.ts
+++ lib/main.ts
@@ -1,27 +1,41 @@
-import express from "express";
+import e from "express";
 import core from "express-serve-static-core";
 import path from "path";
 import { createServer, build, resolveConfig } from "vite";
 import fetch from "node-fetch";
 
-async function serveProduction(app: core.Express) {
-  await build();
-  const config = await resolveConfig({}, "build");
-  app.use(express.static(path.resolve(__dirname, config.build.outDir)));
-}
+const { NODE_ENV } = process.env;
 
-async function serveDevelopment(app: core.Express) {
-  const server = await createServer({ clearScreen: false });
-  const { config } = server;
+const MODE = NODE_ENV === "production" ? NODE_ENV : "development";
+const PORT = 5173;
+const VITE_HOST = `http://127.0.0.1:${PORT}`;
 
-  const PORT = config.server.port ?? 5173;
-  const VITE_HOST = `http://127.0.0.1:${PORT}`;
+function serveStatic() {
+  if (MODE === "production") {
+    return (_: e.Request, __: e.Response, next: e.NextFunction) => next();
+  }
 
-  app.use((req, res, next) => {
+  return (req: e.Request, res: e.Response, next: e.NextFunction) => {
     if (req.path.match(/(\.\w+$)|(@react-refresh|@vite)/))
       return res.redirect(`${VITE_HOST}${req.path}`);
     next();
+  };
+}
+
+async function serveProduction(app: core.Express) {
+  await build();
+  const config = await resolveConfig({}, "build");
+  console.log(path.resolve(__dirname, config.root, config.build.outDir));
+  app.use(e.static(path.resolve(__dirname, config.root, config.build.outDir)));
+}
+
+async function serveDevelopment(app: core.Express) {
+  const server = await createServer({
+    clearScreen: false,
+    server: { port: PORT },
   });
+
+  app.use(serveStatic());
 
   app.get("/*", async (_, res) => {
     const content = await fetch(VITE_HOST).then((res) => res.text());
@@ -32,20 +46,14 @@ async function serveDevelopment(app: core.Express) {
 }
 
 async function serve(app: core.Express) {
-  const mode = process.env.NODE_ENV ?? "development";
-
-  mode === "production"
+  MODE === "production"
     ? await serveProduction(app)
     : await serveDevelopment(app);
 }
 
-async function listen(
-  app: core.Express,
-  port: number = 3000,
-  callback = () => console.log(`Server is listening on port ${port}...`)
-) {
+async function listen(app: core.Express, port: number, callback: () => void) {
   await serve(app);
   app.listen(port, callback);
 }
 
-export = { serve, listen };
+export = { static: serveStatic, serve, listen };
