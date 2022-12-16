52f2c371d5bf758abc905a29a8c01fd7c569f544
Szymon Miśtal <mistalzaq@gmail.com>
2022-12-16 20:18:01 +0100

    Get rid of playground

--- playground/index.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import ViteExpress from "../dist/main";
-
-const app = express();
-
-ViteExpress.listen(app, 3000, () =>
-  console.log("Playground is listening on port 3000...")
-);
