e8483ec07605fc0bc268d913f60c8d9e70c812b6
Szymon Miśtal <mistalzaq@gmail.com>
2023-01-20 11:08:18 +0100

    Go back to using `prettier` for formatting

--- examples/basic-ts-app/src/server.ts
+++ examples/basic-ts-app/src/server.ts
@@ -4,7 +4,7 @@ import ViteExpress from "vite-express";
 const app = express();
 
 ViteExpress.listen(app, 3000, () =>
-  console.log("Server is listening on port 3000..."),
+  console.log("Server is listening on port 3000...")
 );
 
 app.get("/hello", (_, res) => {
