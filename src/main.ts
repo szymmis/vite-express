443568db506ddda1c1f94bda2f9c789eccad13cb
Szymon Miśtal <mistalzaq@gmail.com>
2024-06-08 09:20:23 +0200

    Pass serve-static options throigh `static` function

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import http from "http";
 import https from "https";
 import path from "path";
 import pc from "picocolors";
+import { ServeStaticOptions } from "serve-static";
 import type { HmrOptions, ViteDevServer } from "vite";
 
 type ViteConfig = {
@@ -16,79 +17,12 @@ type ViteConfig = {
 
 const _State = {
   viteConfig: undefined as ViteConfig | undefined,
+  staticOptions: undefined as ServeStaticOptions | undefined,
 };
 
 function clearState() {
   _State.viteConfig = undefined;
 }
-
-type ServeStaticOptions = {
-  /**
-   * Enable or disable setting Cache-Control response header, defaults to true.
-   * Disabling this will ignore the immutable and maxAge options.
-   */
-  cacheControl?: boolean | undefined;
-
-  /**
-   * Set how "dotfiles" are treated when encountered. A dotfile is a file or directory that begins with a dot (".").
-   * Note this check is done on the path itself without checking if the path actually exists on the disk.
-   * If root is specified, only the dotfiles above the root are checked (i.e. the root itself can be within a dotfile when when set to "deny").
-   * The default value is 'ignore'.
-   * 'allow' No special treatment for dotfiles
-   * 'deny' Send a 403 for any request for a dotfile
-   * 'ignore' Pretend like the dotfile does not exist and call next()
-   */
-  dotfiles?: string | undefined;
-
-  /**
-   * Enable or disable etag generation, defaults to true.
-   */
-  etag?: boolean | undefined;
-
-  /**
-   * Set file extension fallbacks. When set, if a file is not found, the given extensions will be added to the file name and search for.
-   * The first that exists will be served. Example: ['html', 'htm'].
-   * The default value is false.
-   */
-  extensions?: string[] | false | undefined;
-
-  /**
-   * Let client errors fall-through as unhandled requests, otherwise forward a client error.
-   * The default value is true.
-   */
-  fallthrough?: boolean | undefined;
-
-  /**
-   * Enable or disable the immutable directive in the Cache-Control response header.
-   * If enabled, the maxAge option should also be specified to enable caching. The immutable directive will prevent supported clients from making conditional requests during the life of the maxAge option to check if the file has changed.
-   */
-  immutable?: boolean | undefined;
-
-  /**
-   * Enable or disable Last-Modified header, defaults to true. Uses the file system's last modified value.
-   */
-  lastModified?: boolean | undefined;
-
-  /**
-   * Provide a max-age in milliseconds for http caching, defaults to 0. This can also be a string accepted by the ms module.
-   */
-  maxAge?: number | string | undefined;
-
-  /**
-   * Redirect to trailing "/" when the pathname is a dir. Defaults to true.
-   */
-  redirect?: boolean | undefined;
-
-  /**
-   * Function to set custom headers on response. Alterations to the headers need to occur synchronously.
-   * The function is called as fn(res, path, stat), where the arguments are:
-   * res the response object
-   * path the file path that is being sent
-   * stat the stat object of the file that is being sent
-   */
-  setHeaders?: // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  ((res: http.ServerResponse, path: string, stat: any) => any) | undefined;
-};
 
 const Config = {
   mode: (process.env.NODE_ENV === "production"
@@ -103,7 +37,6 @@ const Config = {
   transformer: undefined as
     | undefined
     | ((html: string, req: express.Request) => string | Promise<string>),
-  serveStaticOptions: {} as ServeStaticOptions,
 };
 
 type ConfigurationOptions = Partial<typeof Config>;
@@ -218,9 +151,7 @@ async function getDistPath() {
   return path.resolve(config.root, config.build.outDir);
 }
 
-async function serveStatic(
-  options: Partial<ServeStaticOptions>,
-): Promise<RequestHandler> {
+async function serveStatic(): Promise<RequestHandler> {
   const distPath = await getDistPath();
 
   if (!fs.existsSync(distPath)) {
@@ -234,10 +165,7 @@ async function serveStatic(
     info(`${pc.green(`Serving static files from ${pc.gray(distPath)}`)}`);
   }
 
-  return express.static(distPath, {
-    index: false,
-    ...options,
-  });
+  return express.static(distPath, { index: false, ..._State.staticOptions });
 }
 
 const stubMiddleware: RequestHandler = (req, res, next) => next();
@@ -389,7 +317,6 @@ function config(config: ConfigurationOptions) {
   Config.inlineViteConfig = config.inlineViteConfig;
   Config.transformer = config.transformer;
   Config.viteConfigFile = config.viteConfigFile;
-  Config.serveStaticOptions = config.serveStaticOptions ?? {};
 }
 
 async function bind(
@@ -406,10 +333,7 @@ async function bind(
     await injectStaticMiddleware(app, vite.middlewares);
     await injectViteHTMLMiddleware(app, vite);
   } else {
-    await injectStaticMiddleware(
-      app,
-      await serveStatic(Config.serveStaticOptions),
-    );
+    await injectStaticMiddleware(app, await serveStatic());
     await injectHTMLMiddleware(app);
   }
 
@@ -434,6 +358,9 @@ export default {
   bind,
   listen,
   build,
-  static: () => stubMiddleware,
+  static: (options?: ServeStaticOptions) => {
+    _State.staticOptions = options;
+    return stubMiddleware;
+  },
   getViteConfig,
 };
