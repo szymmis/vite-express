74ad182b119d4b08ae29ed9be50b00b67191a678
Shishkevich D. <135337715+shishkevichd@users.noreply.github.com>
2024-09-04 09:49:37 +0000

    chore: update package.json name field to project name

--- create-vite-express/src/patches.ts
+++ create-vite-express/src/patches.ts
@@ -1,15 +1,17 @@
-export const PATCHES: Record<string, (content: string) => string> = {
-  "package.json": (content: string) => {
+export const PATCHES: Record<string, (content: string, variables: Array<String>) => string> = {
+  "package.json": (content: string, variables: Array<String>) => {
+    const json = JSON.parse(content);
+
+    json["name"] = variables[0]
+
     if (process.platform === "win32") {
-      const json = JSON.parse(content);
       json.scripts["start"] = json.scripts["start"].replace(
         "NODE_ENV=production",
         "cross-env NODE_ENV=production",
       );
       json.dependencies["cross-env"] = "^7.0.3";
-      return JSON.stringify(json, null, 2);
-    } else {
-      return content;
-    }
+    } 
+    
+    return JSON.stringify(json, null, 2);
   },
 };
