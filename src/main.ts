381b3a2abd4f92016b455e0c577334870ff591b6
James Lounds <jameslounds@gmail.com>
2024-11-20 13:34:06 +0000

    bring behaviour in line with `vite dev` and `vite preview`. Also includes some changes the precommit hooks forced me to make

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,13 @@
 import express, { RequestHandler } from "express";
 import core from "express-serve-static-core";
+import expressStaticGzip, {
+  ExpressStaticGzipOptions,
+} from "express-static-gzip";
 import fs from "fs";
 import http from "http";
 import https from "https";
 import path from "path";
 import pc from "picocolors";
-import expressStaticGzip, {
-  ExpressStaticGzipOptions,
-} from "express-static-gzip";
-
 import type { HmrOptions, ViteDevServer } from "vite";
 
 type ViteConfig = {
@@ -121,7 +120,7 @@ async function resolveConfig(): Promise<ViteConfig> {
         )} mode`,
       );
     }
-  } catch (e) {
+  } catch {
     /* empty */
   }
 
@@ -139,7 +138,7 @@ async function resolveConfig(): Promise<ViteConfig> {
       base: base ?? defaultConfig.base,
       build: { outDir: outDir ?? defaultConfig.build.outDir },
     };
-  } catch (e) {
+  } catch {
     error(
       `Unable to locate ${pc.yellow(
         "vite.config.*s file",
@@ -195,7 +194,7 @@ async function injectStaticMiddleware(
   const router = (() => {
     try {
       return app.router;
-    } catch (e) {
+    } catch {
       return app._router;
     }
   })();
@@ -226,6 +225,12 @@ function findTemplateFilePath(
 ): string | undefined {
   if (reqPath.endsWith(".html")) {
     const pathToTest = path.join(root, reqPath);
+    if (fs.existsSync(pathToTest)) return pathToTest;
+  }
+
+  // if the path doesn't end with a /, look for root/req/path.html
+  if (!reqPath.endsWith("/")) {
+    const pathToTest = path.join(root, reqPath + ".html");
     if (fs.existsSync(pathToTest)) return pathToTest;
   }
 
@@ -372,7 +377,8 @@ export default {
   listen,
   build,
   static: (options?: ExpressStaticGzipOptions) => {
-    _State.staticOptions = options;
+    // vite does not redirect `/<path>` to `/<path>/`, so neither do we by default
+    _State.staticOptions = { serveStatic: { redirect: false }, ...options };
     return stubMiddleware;
   },
   getViteConfig,
